ed291b3a9ab68dc289d4626344b1801999246931
Daniel  Kim <danp.kim369@gmail.com>
2026-02-03 23:05:12 -0800

    Create MyCalendar abstract class
    Define abstract class MyCalendar with methods for subclasses.

---
+++ MyCalendar.java
@@ -0,0 +1,30 @@
+/*
+ * Name: Daniel Kim
+ * Lab: Exercise 13.4
+ * Date: 2026-02-03
+ */
+
+import java.util.Calendar;
+
+public abstract class MyCalendar {
+
+    protected Calendar calendar;
+
+    protected MyCalendar() {
+        calendar = new java.util.GregorianCalendar();
+    }
+
+    // Abstract methods subclasses must implement
+    public abstract int daysInMonth();
+
+    public abstract String getMonthName();
+
+    // Concrete methods accessible to subclasses and users
+    public int get(int field) {
+        return calendar.get(field);
+    }
+
+    public void set(int field, int value) {
+        calendar.set(field, value);
+    }
+}
